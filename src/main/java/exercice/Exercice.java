7f0db5c846f7a19e50334e3363deeaac61e17a8f
Thomaw <darkskirime@gmail.com>
2023-06-03 15:08:25 +0200

    Update Exercice.java
    ne passe pas les tests

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,10 +1,22 @@
 package exercice;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        throw new UnsupportedOperationException("Unimplemented");
+        String[] mots = str.split(" ");
+        List<String> result = new ArrayList<>();
+
+        for (Character lettre : ordre) {
+            for (String mot : mots) {
+                if (mot.startsWith(lettre.toString())) {
+                    result.add(mot);
+                }
+            }
+        }
+
+        return result;
     }
 }
 
