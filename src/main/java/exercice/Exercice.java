5e34a613323dc48517c09e8d4e767d7527476db3
Thomaw <darkskirime@gmail.com>
2023-06-03 15:07:35 +0200

    Update Exercice.java

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,22 +1,10 @@
 package exercice;
 
 import java.util.List;
-import java.util.ArrayList;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        String[] mots = str.split(" ");
-        List<String> result = new ArrayList<>();
-
-        for (Character lettre : ordre) {
-            for (String mot : mots) {
-                if (mot.startsWith(lettre.toString())) {
-                    result.add(mot);
-                }
-            }
-        }
-
-        return result;
+        throw new UnsupportedOperationException("Unimplemented");
     }
 }
 
